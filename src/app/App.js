6c152af7bee1aacbfe84eab6bda0d57d5cf8384a
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2023-05-11 23:54:58 +0200

    Add website sections - Home, About, Skills, Portfolio and Contact

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Footer from '../layouts/Footer'
 import Header from '../layouts/Header'
-import Home from '../pages/Home'
 
 const App = () => {
     return (
         <div className="fullscreen">
             <Header />
-            <Home />
             <Footer />
         </div>
     )
