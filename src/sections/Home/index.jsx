538be23299be322859354d78442929ea263e4fd3
Lukasz Szymanski <lukasz.szymanski96@gmail.com>
2023-07-12 23:56:16 +0200

    Delete unused files

--- src/sections/Home/index.jsx
+++ src/sections/Home/index.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import Section from '../../components/Section'
-import TypeWriter from '../../components/TypeWriter'
 
 const Home = () => {
   return (
     <Section>
-      <TypeWriter
-        loop={true}
-        text={['Front-end Developer', 'Web Developer', 'Tech Enthusiast']}
-      />
+      Home Section!
     </Section>
   )
 }
